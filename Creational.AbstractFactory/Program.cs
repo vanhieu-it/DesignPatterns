069de10d3da48d3c193c0862c4d463238821e7f6
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-03 14:05:49 +0700

    up Creational.AbstractFactory

---
+++ Creational.AbstractFactory/Program.cs
@@ -0,0 +1,54 @@
+﻿var oracleDatabaseFactory = new OracleDatabaseFactory();
+var connection = oracleDatabaseFactory.CreateConnection();
+var command = oracleDatabaseFactory.CreateCommand(connection);
+
+connection.Connect();
+command.Execute();
+connection.DidConnect();
+
+interface ICommand
+{
+    void Execute();
+}
+interface IConnection
+{
+    void Connect();
+    void DidConnect();
+}
+interface IDatabaseFactory
+{
+    IConnection CreateConnection();
+    ICommand CreateCommand(IConnection connection);
+}
+
+class OracleCommand : ICommand
+{
+    public void Execute()
+    {
+        Console.WriteLine("Oracle running...");
+        Console.WriteLine("Complete");
+    }
+}
+class OracleConnection : IConnection
+{
+    public void Connect()
+    {
+        Console.WriteLine("Connected");
+    }
+
+    public void DidConnect()
+    {
+        Console.WriteLine("Disconnected");
+    }
+}
+class OracleDatabaseFactory : IDatabaseFactory
+{
+    public ICommand CreateCommand(IConnection connection)
+        => connection == null ? throw new NullReferenceException() : new OracleCommand();
+
+    public IConnection CreateConnection() => new OracleConnection();
+}
+
+
+
+
