72164c74294b09a516542db32f7bf4847c07f3bc
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-03 14:59:14 +0700

    add Creational.Builder

--- Creational.Builder/Program.cs
+++ Creational.Builder/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String connectionString = new ConnectionBuilder()
+    .SetDatabase("mysql")
+    .SetServer("127.0.0.1")
+    .SetUserId("sa")
+    .SetPassword("123")
+    .Build();
+
+Console.WriteLine(connectionString);
+
+class ConnectionBuilder
+{
+    private class Connection
+    {
+        public string? database;
+        public string? server;
+        public string? userId;
+        public string? password;
+
+        public override string ToString()
+        {
+            return $"Server={server};\nDatabase={database};\nUserId={userId};\nPassword={password}";
+        }
+    }
+
+    private readonly Connection connection;
+    public ConnectionBuilder()
+    {
+        connection = new Connection();
+    }
+    public ConnectionBuilder SetDatabase(String database)
+    {
+        connection.database = database;
+        return this;
+    }
+    public ConnectionBuilder SetServer(String server)
+    {
+        connection.server = server;
+        return this;
+    }
+    public ConnectionBuilder SetUserId(String userId)
+    {
+        connection.userId = userId;
+        return this;
+    }
+    public ConnectionBuilder SetPassword(String password) {  
+        connection.password = password;
+        return this;
+    }
+    public String Build()
+    {
+        return connection.ToString();
+    }
+}
--- Creational.Builder/obj/Debug/net8.0/Creational.Builder.AssemblyInfo.cs
+++ Creational.Builder/obj/Debug/net8.0/Creational.Builder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Creational.Builder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed76e02bedba1ae71dd9d1b93f73eb8b9a57a06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e2b1f377d982641fa485829303cd6d7cc75e77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Creational.Builder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Creational.Builder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
