b5f6438947c6c1100818d55785102d4156612da7
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-03 15:11:28 +0700

    add Creational.Singleton

--- Creational.Singleton/Program.cs
+++ Creational.Singleton/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿DateTimeSnapshot instance = Singleton.GetInstance();
+
+Console.WriteLine(instance.GetDateTime());
+Thread.Sleep(1000);
+Console.WriteLine(instance.GetDateTime());
+Thread.Sleep(1000);
+Console.WriteLine(instance.GetDateTime());
+Thread.Sleep(1000);
+Console.WriteLine(instance.GetDateTime());
+Thread.Sleep(1000);
+Console.WriteLine(instance.GetDateTime());
+
+class DateTimeSnapshot
+{
+    public string GetDateTime()
+    {
+        return DateTime.Now.ToString();
+    }
+}
+class Singleton
+{
+    private static DateTimeSnapshot instance = new DateTimeSnapshot();
+    public static DateTimeSnapshot GetInstance()
+    {
+        return instance;
+    }
+}
+
+
