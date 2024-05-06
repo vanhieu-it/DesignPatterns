0891e3b20a4ec93ac0371278a72333a1c33e267d
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-06 16:34:56 +0700

    add Structural.Facade

---
+++ Structural.Facade/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Waiter
+{
+    public void TakeOrder() => Console.WriteLine("Waiter takes order");  
+}
+public class Chef
+{
+    public void Cook() => Console.WriteLine("Chef cooks the meal"); 
+}
+public class Cashier
+{
+    public void BillCustomer() => Console.WriteLine("Cashier bills the customer");
+}
+public class RestaurantFacade
+{
+    private Waiter _waiter;
+    private Chef _chef;
+    private Cashier _cashier;
+
+    public RestaurantFacade()
+    {
+        _waiter = new Waiter();
+        _chef = new Chef();
+        _cashier = new Cashier();
+    }
+    public void CompleteOrder()
+    {
+        _waiter.TakeOrder();
+        _chef.Cook();
+        _cashier.BillCustomer();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        RestaurantFacade facade = new RestaurantFacade();
+        facade.CompleteOrder();
+    }
+}
