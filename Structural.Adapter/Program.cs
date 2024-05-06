1a59b7dbf5d7d7d814d1416e8a8e828e12daed83
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-06 14:21:17 +0700

    add Structural.Adapter

---
+++ Structural.Adapter/Program.cs
@@ -0,0 +1,37 @@
+﻿interface ITarget
+{
+    string Request(int i);
+}
+
+class Adaptee
+{
+    public double SpecificRequets(double a, double b)
+    {
+        return a / b;
+    }
+}
+
+//Adapter giữa hai giao diện
+class Adapter : ITarget
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        this._adaptee = adaptee;
+    }
+    public string Request(int i)
+    {
+        return $"Result: {_adaptee.SpecificRequets(i,5)}";
+    }
+}
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        Adaptee adaptee = new Adaptee();
+        ITarget target = new Adapter(adaptee);
+        Console.WriteLine(target.Request(110));
+    }
+}
