3999674b16ccc2077119dff74a5f5daf03f171d5
Văn Hiếu <vanhieu99it@gmail.com>
2024-05-24 09:21:45 +0700

    #26 Mediator

---
+++ Behavioral.Mediator/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behavioral.Mediator
+{
+    public interface IMediator
+    {
+        void Notify(object sender, string eventCode);
+    }
+    public class ConcreteMediator : IMediator
+    {
+        private Component1 _component1;
+        private Component2 _component2;
+
+        public ConcreteMediator(Component1 component1, Component2 component2)
+        {
+            _component1 = component1;
+            _component1.SetMediator(this);
+            _component2 = component2;
+            _component2.SetMediator(this);
+        }
+
+        public void Notify(object sender, string eventCode)
+        {
+            if (eventCode == "A")
+            {
+                Console.WriteLine("Mediator reacts on A and triggers following operations:");
+                _component2.DoC();
+            }
+            else if (eventCode == "D")
+            {
+                Console.WriteLine("Mediator reacts on D and triggers following operations:");
+                _component1.DoB();
+                _component2.DoC();
+            }
+        }
+    }
+    public class BaseComponent
+    {
+        protected IMediator _mediator;
+
+        public BaseComponent(IMediator mediator = null)
+        {
+            _mediator = mediator;
+        }
+
+        public void SetMediator(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+
+    public class Component1 : BaseComponent
+    {
+        public void DoA()
+        {
+            Console.WriteLine("Component 1 does A.");
+            _mediator.Notify(this, "A");
+        }
+
+        public void DoB()
+        {
+            Console.WriteLine("Component 1 does B.");
+        }
+    }
+
+    public class Component2 : BaseComponent
+    {
+        public void DoC()
+        {
+            Console.WriteLine("Component 2 does C.");
+        }
+
+        public void DoD()
+        {
+            Console.WriteLine("Component 2 does D.");
+            _mediator.Notify(this, "D");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component1 component1 = new Component1();
+            Component2 component2 = new Component2();
+            new ConcreteMediator(component1, component2);
+
+            Console.WriteLine("Client triggers operation A.");
+            component1.DoA();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client triggers operation D.");
+            component2.DoD();
+        }
+    }
+
+}
